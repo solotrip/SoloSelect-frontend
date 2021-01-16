35710871da40e4285b4597706ecc066534b5039f
Berk Durmus <berkdurmus@yahoo.com>
2021-01-16 17:57:51 +0300

    See  number of photos of each cities with index.
    See  number of photos of each cities with index.

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ import cityOptions from "./cityoptions.json";
 import axios from "axios";
 
 let photoCount = 0;
+let a;
 
 let cityCount = 0;
 const api = createApi({
@@ -113,6 +114,8 @@ const Body = () => {
   const [selecteds, setSelecteds] = useState([]);
   const [city, setCity] = useState("mexico-city-mexico_Q1489");
   const [imageMode, setImageMode] = useState("city-card");
+  const [serverImages, setServerImages] = useState(null);
+  const [wkdArray, setWkdArray] = useState(null);
 
   const onchange = (data) => {
     //arr.push(data);
@@ -126,6 +129,24 @@ const Body = () => {
       }
     }
   };
+
+  function pushToItemCounter(array) {
+    var obj = [];
+    array.forEach(function (val) {
+      if (!obj[val])
+        // create new property if property is not found
+        obj[val["wkd_id"]] = 1;
+      // increment matched property by 1
+      else obj[val["wkd_id"]]++;
+    });
+    console.log("object is:", obj);
+  }
+
+  async function getFromBackend() {
+    const photos = await axios.get("http://localhost:5001/photos");
+    //console.log("photos count: ", photos.data);
+    return photos.data;
+  }
 
   async function sendToBackend(
     id,
@@ -150,6 +171,18 @@ const Body = () => {
       content: content,
     });
   }
+
+  const getImages = () => {
+    let images = [];
+    setTimeout(() => {
+      getFromBackend().then((x) => {
+        console.log("x is ", x);
+        setServerImages(x);
+
+        return x;
+      });
+    }, 100);
+  };
 
   const uploadFile = () => {
     selecteds.forEach((selected) => {
@@ -282,6 +315,10 @@ const Body = () => {
 
   const handleSelectedChange = () => {};
 
+  let objHolder = {};
+
+  let objcount = 0;
+
   useEffect(() => {
     api.search
       .getPhotos({ query: query, perPage: 30 })
@@ -306,112 +343,115 @@ const Body = () => {
     );
   } else {
     return (
-      <div className="colHolder">
-        <div className="leftCol">
-          <form
-            className="form"
-            onSubmit={(e) => searchPhotosv2(e, currentPage, orientation)}
-          >
-            <label className="label" htmlFor="query">
-              SoloSelect 📷
-            </label>
-            <input
-              type="text"
-              name="query"
-              className="input"
-              placeholder={`Try "Oslo" or "sky"`}
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-            />
-            <select
-              value={orientation}
-              onChange={(e) => setOrientation(e.target.value)}
+      <>
+        <div className="colHolder">
+          <div className="leftCol">
+            <form
+              className="form"
+              onSubmit={(e) => searchPhotosv2(e, currentPage, orientation)}
             >
-              <option value="all">All</option>
-              <option value="landscape">Landscape</option>
-              <option value="portrait">Portrait</option>
-              <option value="squarish">Squarish</option>
-            </select>
-            <button type="submit" className="button">
-              Search
-            </button>
-            {/*<Dropdown
+              <label className="label" htmlFor="query">
+                SoloSelect 📷
+              </label>
+              <input
+                type="text"
+                name="query"
+                className="input"
+                placeholder={`Try "Oslo" or "sky"`}
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <select
+                value={orientation}
+                onChange={(e) => setOrientation(e.target.value)}
+              >
+                <option value="all">All</option>
+                <option value="landscape">Landscape</option>
+                <option value="portrait">Portrait</option>
+                <option value="squarish">Squarish</option>
+              </select>
+              <button type="submit" className="button">
+                Search
+              </button>
+              {/*<Dropdown
             selection
             fluid
             options={orientationOptions}
             placeholder="Choose an orientation option"
           />*/}
-          </form>
-          <button
-            onClick={(e) => {
-              if (currentPage > 0) {
-                setCurrentPage(currentPage - 1);
-                searchPhotosv2(e, currentPage - 1, orientation);
-              }
-            }}
-          >
-            Previous Page
-          </button>
-          Page {currentPage} of {maxPage}
-          <button
-            onClick={(e) => {
-              if (currentPage < maxPage) {
-                setCurrentPage(currentPage + 1);
-                searchPhotosv2(e, currentPage + 1, orientation);
-              }
-            }}
-          >
-            Next Page
-          </button>
-          <div className="feed">
-            <ul className="columnUl">
-              {data.map((photo) => (
-                <li key={photo.id} className="li">
-                  <PhotoComp
-                    photo={photo}
-                    width={photo.width}
-                    height={photo.height}
-                    index={photoCount}
-                    photoId={photo.id}
-                    selectedImages={selectedImages}
-                    onchange={(e) => {
-                      onchange(e);
-                      console.log("at child: ", selecteds);
-                    }}
-                    data={selecteds}
-                  />
-                  {/*(photoCount = photoCount + 1)*/}
-                </li>
-              ))}
-            </ul>
+            </form>
+            <button
+              onClick={(e) => {
+                if (currentPage > 0) {
+                  setCurrentPage(currentPage - 1);
+                  searchPhotosv2(e, currentPage - 1, orientation);
+                }
+              }}
+            >
+              Previous Page
+            </button>
+            Page {currentPage} of {maxPage}
+            <button
+              onClick={(e) => {
+                if (currentPage < maxPage) {
+                  setCurrentPage(currentPage + 1);
+                  searchPhotosv2(e, currentPage + 1, orientation);
+                }
+              }}
+            >
+              Next Page
+            </button>
+            <div className="feed">
+              <ul className="columnUl">
+                {data.map((photo) => (
+                  <li key={photo.id} className="li">
+                    <PhotoComp
+                      photo={photo}
+                      width={photo.width}
+                      height={photo.height}
+                      index={photoCount}
+                      photoId={photo.id}
+                      selectedImages={selectedImages}
+                      onchange={(e) => {
+                        onchange(e);
+                        console.log("at child: ", selecteds);
+                      }}
+                      data={selecteds}
+                    />
+                    {/*(photoCount = photoCount + 1)*/}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <button
+              onClick={(e) => {
+                if (currentPage > 0) {
+                  setCurrentPage(currentPage - 1);
+                  searchPhotosv2(e, currentPage - 1, orientation);
+                }
+              }}
+            >
+              Previous Page
+            </button>
+            Page {currentPage} of {maxPage}
+            <button
+              onClick={(e) => {
+                if (currentPage < maxPage) {
+                  setCurrentPage(currentPage + 1);
+                  searchPhotosv2(e, currentPage + 1, orientation);
+                }
+              }}
+            >
+              Next Page
+            </button>
           </div>
-          <button
-            onClick={(e) => {
-              if (currentPage > 0) {
-                setCurrentPage(currentPage - 1);
-                searchPhotosv2(e, currentPage - 1, orientation);
-              }
-            }}
-          >
-            Previous Page
-          </button>
-          Page {currentPage} of {maxPage}
-          <button
-            onClick={(e) => {
-              if (currentPage < maxPage) {
-                setCurrentPage(currentPage + 1);
-                searchPhotosv2(e, currentPage + 1, orientation);
-              }
-            }}
-          >
-            Next Page
-          </button>
-        </div>
-        <div className="rightCol">
-          <div className="selectedImages">Selected Images:{selectedImages}</div>
-          <br />
-          Which city to upload?
-          {/*
+          <div className="rightCol">
+            <div className="selectedImages">
+              Selected Images:{selectedImages}
+            </div>
+            <br />
+            Which city to upload?
+            {/*
           <select
             className="selector"
             onChange={(e) => {
@@ -426,33 +466,62 @@ const Body = () => {
             ))}
           </select>
           */}
-          <Select
-            value={city}
-            onChange={(e) => {
-              setCity(e.label);
-              console.log("e is ", e.label);
-              console.log("city changed to:", city);
-            }}
-            isSearchable={true}
-            options={cityOptions}
-          />
-          <br />
-          <br />
-          As a City Card or Gallery(Carousel) ?
-          <select
-            className="selector2"
-            value={imageMode}
-            onChange={(e) => setImageMode(e.target.value)}
-          >
-            <option value="city-card">City Card</option>
-            <option value="gallery">Gallery</option>
-          </select>
-          <br />
-          <button className="uploadButton" onClick={uploadFile}>
-            Upload Selected Images
-          </button>{" "}
+            <Select
+              value={city}
+              onChange={(e) => {
+                setCity(e.label);
+                console.log("e is ", e.label);
+                console.log("city changed to:", city);
+              }}
+              isSearchable={true}
+              options={cityOptions}
+            />
+            <br />
+            <br />
+            As a City Card or Gallery(Carousel) ?
+            <select
+              className="selector2"
+              value={imageMode}
+              onChange={(e) => setImageMode(e.target.value)}
+            >
+              <option value="city-card">City Card</option>
+              <option value="gallery">Gallery</option>
+            </select>
+            <br />
+            <button className="uploadButton" onClick={uploadFile}>
+              Upload Selected Images
+            </button>{" "}
+          </div>
         </div>
-      </div>
+        <div className="count-section">
+          City Ids with Number of Photos
+          <div className="to-do">
+            <button className="uploadButton" onClick={getImages}>
+              Click to See Photos
+            </button>
+            {/*serverImages && aggregateWkds(serverImages)*/}
+            {serverImages && pushToItemCounter(serverImages)}
+            {serverImages &&
+              serverImages.map((photo) => {
+                !objHolder[photo.wkd_id]
+                  ? (objHolder[photo.wkd_id] = 1)
+                  : objHolder[photo.wkd_id]++;
+              })}
+
+            {Object.keys(objHolder).map((item, key) => (
+              <li key={item} className="li">
+                <div />
+                <div className="colored-text-holder">
+                  <div className="olive-text">{item}</div>
+                  <div className="orange-text">
+                    {Object.values(objHolder)[key]}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
 };
